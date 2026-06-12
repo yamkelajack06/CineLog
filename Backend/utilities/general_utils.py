7ddee33f115a76a69d8325136433c978cc72e0d9
Yamkela Jack <yamkelajack06@gmail.com>
2026-06-13 01:19:47 +0200

    txt requirements update:

--- Backend/utilities/general_utils.py
+++ Backend/utilities/general_utils.py
@@ -1,7 +1,7 @@
 from passlib.context import CryptContext
 import uuid
 
-pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
+pwd_context = CryptContext(schemes=["argon2"], deprecated="auto")
 
 class General_Utils:
     @staticmethod
