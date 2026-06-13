c18287b0aca6afcb48eaf47fe78c7c7f79e5cde7
Yamkela Jack <yamkelajack06@gmail.com>
2026-06-13 19:53:38 +0200

    fixed cors

--- Backend/main.py
+++ Backend/main.py
@@ -11,8 +11,8 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"], 
-    allow_credentials=True,
+    allow_origins=["*"],
+    allow_credentials=False,
     allow_methods=["*"],
     allow_headers=["*"],
 )
